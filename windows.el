0945550a207c62fe0cba2caf52ee5b14aa93e21c
Nicolas O <nicol@LAPTOP-E8GSO46C>
2025-11-16 12:45:33 +0100

    Correct typo

--- windows.el
+++ windows.el
@@ -2,7 +2,7 @@
 
 (defun add-to-environment-variable (envt-variable-name prog-name directory)
    "Add DIRECTORY corresponding to PROG-NAME to environment variable ENVT-VARIABLE-NAME. DIRECTORY may have a final slash.
-(v1, available in occisn/elisp-utils GitHub repository)"
+(v1, available in occisn/emacs-utils GitHub repository)"
    (let ((envt-variable-content (getenv envt-variable-name)))
      (if (cl-search directory envt-variable-content)
 	 (message "No need to add %s to Windows %s environment variable since already in: %s" prog-name envt-variable-name directory)
